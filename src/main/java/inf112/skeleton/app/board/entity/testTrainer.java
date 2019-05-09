a3dbf660a7cd5281823ff2793b57e14a8d863825
Promises <henning.ber@gmail.com>
2019-05-10 01:46:54 +0200

    created class testTrainer

---
+++ src/main/java/inf112/skeleton/app/board/entity/testTrainer.java
@@ -0,0 +1,26 @@
+package inf112.skeleton.app.board.entity;
+
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class testTrainer extends Entity {
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+
+    }
+
+    @Override
+    public void renderName(SpriteBatch batch, float scale) {
+
+    }
+
+    @Override
+    protected boolean isBlocking() {
+        return false;
+    }
+}
